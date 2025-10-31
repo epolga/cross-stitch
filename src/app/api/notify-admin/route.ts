830d6ffa351e856c7bdc5ede0a738e1acefc4dbd
Olga <olga.epstein@gmail.com>
2025-10-31 19:20:54 +0200

    Notify when design downloaded

--- src/app/api/notify-admin/route.ts
+++ src/app/api/notify-admin/route.ts
@@ -11,18 +11,19 @@ export async function POST(req: NextRequest) {
       // No body provided or invalid JSON; proceed to default behavior
     }
     console.log("Notify admin called with body:", body);
-    if (body && body.subject && body.message) {
-      // Use provided subject and message for custom notification
-      console.log('Sending custom admin notification with subject:', body.subject);
-      await sendEmailToAdmin(body.subject, body.message, false);
-    } else {
-      console.log('Sending default admin notification for registration form opened');
-      // Default behavior: Notification for registration form opened
-      // Extract the client's IP from headers (handles proxies like Vercel or Cloudflare)
+    // Extract the client's IP from headers (handles proxies like Vercel or Cloudflare)
       const xForwardedFor = req.headers.get('x-forwarded-for');
       const clientIp = xForwardedFor
         ? xForwardedFor.split(',')[0].trim()  // Take the first IP in the chain (client's original IP)
         : req.headers.get('x-real-ip') || 'Unknown';  // Fallback for direct or other headers
+    if (body && body.subject && body.message) {
+      // Use provided subject and message for custom notification
+      console.log('Sending custom admin notification with subject:', body.subject);
+      await sendEmailToAdmin(body.subject, `${body.message}\n Client IP: ${clientIp}`, false);
+    } else {
+      console.log('Sending default admin notification for registration form opened');
+      // Default behavior: Notification for registration form opened
+      
       console.log("Client IP detected as:", clientIp);
       if (!clientIp.startsWith('66.249.')) { // Ignore Googlebot IPs      
         // Send the email with the IP included
